535dbc49745af5a8c1caa42cc79a0125ec99b216
hyperxpizza <wojtek.frackowski@gmail.com>
2021-05-24 21:42:21 +0200

    add user model

---
+++ server/models/users.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type User struct {
+	ID           int       `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"passwordHash"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}

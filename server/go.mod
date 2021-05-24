8b1639731f21445d1b9d3ac3af331da4e336855d
hyperxpizza <wojtek.frackowski@gmail.com>
2021-05-24 22:05:29 +0200

    new user data validator

--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/goware/emailx v0.2.0 // indirect
 	github.com/lib/pq v1.10.2
 	gopkg.in/yaml.v2 v2.4.0
 )
--- server/utils/auth_validator.go
+++ server/utils/auth_validator.go
@@ -1,1 +1,31 @@
 package utils
+
+import (
+	"fmt"
+
+	"github.com/bootkemp-dev/datacat-backend/models"
+	"github.com/goware/emailx"
+)
+
+//Validates the request data from Register endpoint
+func NewUserValidator(user models.RegisterRequest) error {
+	//check length
+	if len(user.Username) <= 0 || len(user.Username) > 50 {
+		return fmt.Errorf("Username not valid")
+	}
+
+	//regex the username
+	err := emailx.Validate(user.Email)
+	if err != nil {
+		if err == emailx.ErrInvalidFormat {
+			return fmt.Errorf("Invalid email format")
+		}
+		return fmt.Errorf("Email validation failed")
+	}
+
+	if user.Password1 != user.Password2 {
+		return fmt.Errorf("Passwords do not match")
+	}
+
+	return nil
+}

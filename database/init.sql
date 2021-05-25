cc21ecf2b94b2198c0baeb6b3f46fd49f9b6351d
hyperxpizza <wojtek.frackowski@gmail.com>
2021-05-25 23:40:30 +0200

    add unique attribute

--- database/init.sql
+++ database/init.sql
@@ -1,8 +1,8 @@
 
 create table users (
-    id serial primary key not null,
-    username varchar(50) not null,
-    email varchar(100) not null,
+    id serial primary key unique not null,
+    username varchar(50) unique not null,
+    email varchar(100) unique not null,
     passwordHash text not null,
     created timestamp not null,
     modified timestamp not null

daa0dc392b87a92c6d3e8cd8323b31d7f8fdd100
hyperxpizza <wojtek.frackowski@gmail.com>
2021-06-02 12:58:37 +0200

    new pool and new job functions

--- server/monitoring/monitoring.go
+++ server/monitoring/monitoring.go
@@ -7,6 +7,37 @@ import (
 	"time"
 )
 
+func NewPool() Pool {
+	return Pool{jobs: []Job{}}
+}
+
+type Pool struct {
+	jobs []Job
+}
+
+func (p Pool) AddJob(j Job) {
+	p.jobs = append(p.jobs, j)
+}
+
+func (p Pool) GetJob(jobID int, userID float64) (*Job, error) {
+	return nil, fmt.Errorf("Job not found")
+}
+
+func NewJob(jobId int, userID float64, name, url string, freq time.Duration) Job {
+	j := Job{
+		JobID:     jobId,
+		UserID:    userID,
+		Name:      name,
+		URL:       url,
+		Frequency: freq,
+		status:    "NA",
+		running:   false,
+		done:      make(chan bool),
+	}
+
+	return j
+}
+
 type Job struct {
 	JobID     int
 	UserID    float64
@@ -19,17 +50,17 @@ type Job struct {
 }
 
 func (j Job) Run() {
-	log.Printf("Starting job | ID: %d | Name: %s\n", j.JobID, j.Name, j.URL)
+	log.Printf("Starting job | ID: %d | Name: %s\n", j.JobID, j.Name)
 	go func() {
 		for {
 			select {
 			case <-j.done:
 				j.running = false
-				log.Printf("Ending job | ID: %d | Name: %s | URL: %s\n", j.JobID)
+				log.Printf("Ending job | ID: %d | Name: %s | URL: %s\n", j.JobID, j.Name, j.URL)
 				return
 			default:
 				log.Printf("Job with ID: %d checking status of: %s ", j.JobID, j.URL)
-				err := j.Status()
+				err := j.URLStatus()
 				if err != nil {
 					j.status = "DOWN"
 					log.Println(err)
@@ -42,7 +73,7 @@ func (j Job) Run() {
 	}()
 }
 
-func (j Job) Status() error {
+func (j Job) URLStatus() error {
 	resp, err := http.Get(j.URL)
 	if err == nil && resp.StatusCode == 200 {
 		return nil
@@ -57,4 +88,10 @@ func (j Job) Stop() error {
 	} else {
 		j.done <- true
 	}
+
+	return nil
+}
+
+func (j Job) GetStatus() string {
+	return j.status
 }

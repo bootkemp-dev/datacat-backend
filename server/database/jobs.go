e624d2bbacf365cd9fae996858ee524e69c6aa97
hyperxpizza <wojtek.frackowski@gmail.com>
2021-06-02 13:32:11 +0200

    fix error handling in insertjob function

--- server/database/jobs.go
+++ server/database/jobs.go
@@ -5,18 +5,18 @@ import (
 	"time"
 )
 
-func InsertNewJob(name, url string, frequency uint, userid float64) (int, error) {
+func InsertNewJob(name, url string, frequency int64, userid float64) (int, error) {
 	stmt, err := db.Prepare(`insert into jobs(id, jobName, jobUrl, frequency, userid, created, modified) values(default, $1, $2, $3, $4, $5, $6) returning id`)
 	if err != nil {
 		log.Println(err)
-		return 0, nil
+		return 0, err
 	}
 
 	var id int
 	err = stmt.QueryRow(name, url, frequency, userid, time.Now(), time.Now()).Scan(&id)
 	if err != nil {
 		log.Println(err)
-		return 0, nil
+		return 0, err
 	}
 
 	return id, nil

fcdff57abcc8db3fe8c987b4fee7f665fef5cecb
hyperxpizza <wojtek.frackowski@gmail.com>
2021-07-02 15:16:06 +0200

    add writing to file

--- server/logger/logger.go
+++ server/logger/logger.go
@@ -1,8 +1,11 @@
 package logger
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 type Logger struct {
@@ -18,4 +21,23 @@ func NewLogger(name, filepath string) (*Logger, error) {
 	}
 
 	return &Logger{name: name, file: file}, nil
+}
+
+func (l *Logger) prepareMessage(message string) string {
+	timeNow := time.Now()
+	timeFormatted := timeNow.Format("2006-01-02 15:04:05")
+
+	return fmt.Sprintf("[%s] %s", timeFormatted, message)
+}
+
+func (l *Logger) WriteLogToFile(message string) error {
+	preparedMessage := l.prepareMessage(message)
+	w := bufio.NewWriter(l.file)
+	_, err := w.WriteString(preparedMessage)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	w.Flush()
+	return nil
 }
--- server/mailing/password_reset.go
+++ server/mailing/password_reset.go
@@ -1,5 +1,18 @@
 package mailing
 
-func SendResetPasswordEmail(username, toEmail, token string) {
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+)
 
+func SendResetPasswordEmail(username, toEmail, token string) {
+	auth := smtp.PlainAuth("", fromEmail, password, smtpHost)
+	toList := []string{toEmail}
+	msg := fmt.Sprintf("To reset your password follow this url: %s", "https://datacat.pl/accounts/reset_password?username="+username+"&token="+token)
+	body := []byte(msg)
+	err := smtp.SendMail(fmt.Sprintf("%s:%d", smtpHost, smtpPort), auth, fromEmail, toList, body)
+	if err != nil {
+		log.Println(err)
+	}
 }
